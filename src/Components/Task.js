2a39054cb6367e0cb972c5c107c7e54d1a2114ed
Giuliano Maradei <72819676+giulianomaradei@users.noreply.github.com>
2023-04-05 18:14:18 -0300

    teste

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Task.module.css'
+import trash from '../Assets/Images/trash.png'
 
 function Task(props){
 
@@ -10,6 +11,11 @@ function Task(props){
         <div className={styles.task}>
             <div className={styles.title}>{title}</div>
             <div className={styles.description}>{description}</div>
+            <div className={styles.buttonSection}>
+                <button className={styles.done}></button>
+                <button className={styles.delete}><img className={styles.deleteImage} src={trash} alt="trash"></img></button>
+            </div>
+     
         </div>
     )
 }
